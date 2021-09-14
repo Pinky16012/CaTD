e44f53aea8383df90a53e053f6bf597b351cb04d
COMPAL\Pinky_Shen <kyis8804@gmail.com>
2021-09-14 11:09:15 +0800

    add catd Model

---
+++ src/catdModels.tsx
@@ -0,0 +1,6 @@
+
+export interface StoredFieldReferences {
+    sdField: string,    // start date field
+    tdField: string,    // target date field
+    estField: string   // est working day field - custom field
+}
